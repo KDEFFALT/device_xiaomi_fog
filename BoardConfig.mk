603584e6f06958d333eb5eab4da3bb9a445b5d7a
Jackie <108690954+KDEFFALT@users.noreply.github.com>
2024-01-02 23:24:07 +0700

    fog: adapt to android 11
    Co-Authored-By: Notkeret <29781314+Notkerd69@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,9 +91,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/frame
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
-ODM_MANIFEST_SKUS += c3qn
-ODM_MANIFEST_C3QN_FILES := $(DEVICE_PATH)/configs/vintf/manifest_c3qn.xml
-
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_fog
 TARGET_RECOVERY_DEVICE_MODULES := libinit_fog
