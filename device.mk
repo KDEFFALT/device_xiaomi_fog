926a230ef6d6af6533b7ad0cdc3557ab10446538
Jackie <108690954+KDEFFALT@users.noreply.github.com>
2024-01-02 15:29:18 +0700

    fog: init adapt to android 11

--- device.mk
+++ device.mk
@@ -115,9 +115,9 @@ TARGET_SCREEN_WIDTH := 720
 
 # Bootcontrol
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
